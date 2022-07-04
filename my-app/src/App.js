fba6a8db509d6cf94070dae8f76783e09837ae51
Nirbhay <hanjuranirbhay@gmail.com>
2022-07-05 00:24:22 +0530

    cards

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,19 +3,19 @@ import Footer from './Footer';
 import Header from './Header';
 import Notes from './Note';
 import Card from "./Card"
-function createEntry(item)
-{
-  return <Card
-    key={item.id}
-    title={item.title}
-    content={item.content}
-  />
-}
+
 function App() {
   return (
     <div className="App">
       <Header/>
-      {Notes.map(createEntry)}
+      {Notes.map((item) =>
+      {
+        return <Card
+          key={item.id}
+          title={item.title}
+          content={item.content}
+        />
+      })}
       <Footer/>
     </div>
   );
