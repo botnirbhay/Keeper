eac5daaf139ce32ff079e50f566e02afca00d446
Nirbhay <hanjuranirbhay@gmail.com>
2022-07-05 00:09:57 +0530

    crds

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,15 @@ import './App.css';
 import Footer from './Footer';
 import Header from './Header';
 import Note from './Note';
-
+import card from "./card"
+function createEntry(item)
+{
+  return <Card
+    key={item.id}
+    name={item.name}
+    content={item.content}
+  />
+}
 function App() {
   return (
     <div className="App">
---
+++ my-app/src/Card.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Card(props)
+{
+    return <div className="note">
+    <h1>{props.name}</h1>
+    <p> {props.content}</p>
+ </div>  
+}
+export default Card;
